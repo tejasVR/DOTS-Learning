d9081402304d824f3774de74fdf20a0d2864703f
tejasVR <tejasshroff24@gmail.com>
2020-04-28 17:57:35 -0400

    Putting the Sheep example in a separate Unity project

---
+++ DOTS Sheep/Assets/Scripts/Sheep Example/ECSMoveSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+public class ECSMoveSystem : JobComponentSystem
+{
+    // An implenented method because we are inheriting from JobComponentSystem
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        // Puts all entities in a group called 'MoveSystem'
+        var jobHandle = Entities.WithName("MoveSystem")
+            // ForEach gets all entities with specific components
+            .ForEach((ref Translation position, ref Rotation rotation) =>
+            {
+                position.Value.y += 0.05F;
+                if (position.Value.y > 100)
+                {
+                    position.Value.y = 0;
+                }                
+            })
+
+            .Schedule(inputDeps);
+
+        return jobHandle;
+    }
+}
