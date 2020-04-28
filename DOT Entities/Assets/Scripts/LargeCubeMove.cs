61386faf99e85b617a8cfdef0d310c356f1c59df
tejasVR <tejasshroff24@gmail.com>
2020-04-28 19:29:59 -0400

    going through the process of debugging entities

---
+++ DOT Entities/Assets/Scripts/LargeCubeMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class LargeCubeMove : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var jobHandle = Entities
+            .WithName("MoveSystem")
+            .ForEach((ref Translation position, ref Rotation rotation, ref LargeCubeData largeCube) =>
+            {
+                position.Value -= 0.01F * math.up();
+            })
+            .Schedule(inputDeps);
+
+        return jobHandle;
+    }
+}
---
+++ DOT Entities/Assets/Scripts/MoveSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+public class MoveSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var jobHandle = Entities
+            .WithName("MoveSystem")
+
+            // Gets all entities with SheepData
+            .ForEach((ref Translation position, ref Rotation rotation, ref SheepData sheepData) =>
+            {
+                // Moving objects up
+                position.Value += 0.01F * math.up();
+            })
+            .Schedule(inputDeps);
+
+        return jobHandle;
+    }
+}
