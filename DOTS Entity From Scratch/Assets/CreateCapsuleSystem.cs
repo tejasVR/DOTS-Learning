202a12ac23932bc9fdf289f2b5bb97f03c077d36
tejasVR <tejasshroff24@gmail.com>
2020-05-04 19:58:43 -0400

    Adding entities from scratch' project

---
+++ DOTS Entity From Scratch/Assets/CreateCapsuleSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+public class CreateCapsuleSystem : JobComponentSystem
+{
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+
+        // Create an instance with these components
+        var instance = EntityManager.CreateEntity(
+            ComponentType.ReadOnly<LocalToWorld>(),   // LocalTo World Component
+            ComponentType.ReadOnly<RenderMesh>()
+            );
+
+        // Give components values
+        EntityManager.SetComponentData(instance,
+            new LocalToWorld
+            {
+                Value = new float4x4(rotation: quaternion.identity, translation: new float3(0, 0, 0))
+            });
+
+        // gets the ResourceLoader script from the ResourceLoader GameObject in our Resources folder
+        var rHolder = Resources.Load<GameObject>("ResourceHolder").GetComponent<ResourceHolder>();
+        EntityManager.SetSharedComponentData(instance,
+            // Adding new MeshRenderer with mesh and material values
+            new RenderMesh
+            {
+                mesh = rHolder.entityMesh,
+                material = rHolder.entityMaterial
+            });
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        return inputDeps;
+    }
+}
---
+++ DOTS Entity From Scratch/Assets/ResourceHolder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceHolder : MonoBehaviour
+{
+    // Holds some rendering values for our entity
+    public Mesh entityMesh;
+    public Material entityMaterial;
+}
