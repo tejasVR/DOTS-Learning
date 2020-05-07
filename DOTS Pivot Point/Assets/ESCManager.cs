bfe6d4d9619cf6475ff26005f26c8c64a59acf4c
tejasVR <tejasshroff24@gmail.com>
2020-05-06 23:07:07 -0400

    Adding pivot point project

---
+++ DOTS Pivot Point/Assets/ESCManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class ESCManager : MonoBehaviour
+{
+    EntityManager entityManger;
+    public GameObject planetPrefab;
+    const int numPlanets = 10000;
+
+    private void Start()
+    {
+        // Define EntityManager, world settings, and convert prefab into entity
+        entityManger = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(planetPrefab, settings);
+
+        for (int i = 0; i < numPlanets; i++)
+        {
+            var instance = entityManger.Instantiate(prefab);
+
+            float x = UnityEngine.Random.Range(-70, 70);
+            float z = UnityEngine.Random.Range(-70, 70);
+
+            entityManger.SetComponentData(instance, new Translation { Value = new float3(x, 0, z) });            
+        }
+    }
+}
---
+++ DOTS Pivot Point/Assets/MoveSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class MoveSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var target = new float3(0, 0, 0);
+        var deltaTime = Time.DeltaTime;
+
+        var moveSpeed = 11F;
+
+        var jobHandle = Entities
+            .WithName("MoveSystem")
+            .ForEach((ref Translation position, ref Rotation rotation, ref PlanetData planetData) =>
+            {
+                var heading = target - position.Value;
+                float3 pivot = target;
+
+                // moves the planets by the inverse of how close the planet is to the pivot
+                float rotationSpeed = deltaTime * moveSpeed * 1 / math.distance(position.Value, pivot);
+
+                // moves an object around a pivot using math.mul
+                position.Value = math.mul(quaternion.AxisAngle(new float3(0, 1, 0), rotationSpeed), position.Value - pivot) + pivot;
+            })
+        .Schedule(inputDeps);
+
+        return jobHandle;
+    }
+}
